589824ee7a6985f793b34323c9db6d7d00cdafce
Christian Witts <cwitts@gmail.com>
2019-02-26 12:15:37 +0200

    Add max_depth param EXTRA_ARGS for large repos
    Signed-off-by: Christian Witts <cwitts@gmail.com>

--- truffleHog/entrypoint.sh
+++ truffleHog/entrypoint.sh
@@ -3,8 +3,18 @@ set -e
 
 cd "${TH_ACTION_WORKING_DIR:-.}"
 
+if [ -z "${TH_MAX_DEPTH}" ]; then
+    EXTRA_ARGS="--max_depth 1000"
+else
+    if [ "$TH_MAX_DEPTH" = "all" ]; then
+        EXTRA_ARGS=""
+    else
+        EXTRA_ARGS="--max_depth $TH_MAX_DEPTH"
+    fi
+fi
+
 set +e
-WARNINGS=$(trufflehog . 2>&1)
+WARNINGS=$(trufflehog "$EXTRA_ARGS" . 2>&1)
 SUCCESS=$?
 echo "$WARNINGS"
 set -e
@@ -21,7 +31,7 @@ COMMENT="#### \`trufflehog\` Failed
 $WARNINGS
 "
 PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
-COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request_comments_url)
+COMMENTS_URL=$(jq -r .pull_request_comments_url < /github/workflow/event.json)
 curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
 
 exit $SUCCESS
