10801a08c64a6025690101bfcefaa37e52a5ad2d
Christian Witts <cwitts@gmail.com>
2019-02-27 18:09:13 +0200

    Add variable to override failure to neutral (78)
    Signed-off-by: Christian Witts <cwitts@gmail.com>

--- java/jdk8/entrypoint.sh
+++ java/jdk8/entrypoint.sh
@@ -29,5 +29,8 @@ PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
 COMMENTS_URL=$(jq -r .pull_request.comments_url /github/workflow/event.json)
 curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
 
-exit $SUCCESS
+if [ -z "${ACTION_FAILURE_CODE}" ]; then
+  exit "$SUCCESS"
+fi
 
+exit "$ACTION_FAILURE_CODE"
