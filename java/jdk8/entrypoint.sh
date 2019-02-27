ea3c10a391532b8da8bd0e5ac7ea9f5ebc7b3684
Christian Witts <cwitts@gmail.com>
2019-02-27 17:05:56 +0200

    Fixup output redirection
    Signed-off-by: Christian Witts <cwitts@gmail.com>

--- java/jdk8/entrypoint.sh
+++ java/jdk8/entrypoint.sh
@@ -6,7 +6,7 @@ cd "${JDK_ACTION_WORKING_DIR:-.}"
 GRADLE_TASK=${GRADLE_ACTION_TASK:-build}
 
 set +e
-WARNINGS=$(./gradlew "${GRADLE_TASK}")
+WARNINGS=$(./gradlew "${GRADLE_TASK}" 2>&1)
 SUCCESS=$?
 echo "$WARNINGS"
 set -e
@@ -15,7 +15,7 @@ if [ $SUCCESS -eq 0 ]; then
     exit 0
 fi
 
-if [ "$SNYK_ACTION_COMMENT" = "1" ] || [ "$SNYK_ACTION_COMMENT" = "false" ]; then
+if [ "$GRADLE_ACTION_COMMENT" = "1" ] || [ "$GRADLE_ACTION_COMMENT" = "false" ]; then
     exit $SUCCESS
 fi
 
@@ -23,7 +23,7 @@ COMMENT="#### \`gradle\` Failed
 $WARNINGS
 "
 PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
-COMMENTS_URL=$(jq -r .pull_request_comments_url < /github/workflow/event.json)
+COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request_comments_url)
 curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
 
 exit $SUCCESS
